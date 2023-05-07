d768beb36444a72bef0664b46e5e33479950aed5
Beso Kiguradze <bkiguradze@gmail.com>
2023-05-07 20:33:36 +0400

    Updates

---
+++ precache-manifest.44cfb860a6a23943746704377abfd9ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a862c37d8f35bf6885dec0df2ecf9739",
+    "url": "/framer-motion-pizza-order/index.html"
+  },
+  {
+    "revision": "94ac66ab6434c1bbe31d",
+    "url": "/framer-motion-pizza-order/static/css/main.5329da44.chunk.css"
+  },
+  {
+    "revision": "6332201cb8808be26429",
+    "url": "/framer-motion-pizza-order/static/js/2.ebc2c67c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/framer-motion-pizza-order/static/js/2.ebc2c67c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ac66ab6434c1bbe31d",
+    "url": "/framer-motion-pizza-order/static/js/main.03f497ff.chunk.js"
+  },
+  {
+    "revision": "2d95e4f16f12e7005b94",
+    "url": "/framer-motion-pizza-order/static/js/runtime-main.27f7390e.js"
+  }
+]);
